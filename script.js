71f21c20b91455222de8d3e5169702906fd96b1f
Mahera Nayan <136584376+itznayan@users.noreply.github.com>
2024-03-25 21:10:14 +0530

    addded transitions
    New animation and transition added

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -8,8 +20,12 @@ function openLinkInNewTab(url) {
   window.open(url, "_blank");
 }
 
+var typed = new Typed("#element", {
+  strings: ["Frontend  Developer", " Android Developer"],
+  typeSpeed: 110,
+  loop: Infinity,
+});
+
 // Example usage
 var linkUrl = "https://www.example.com";
 openLinkInNewTab(linkUrl);
-
-
