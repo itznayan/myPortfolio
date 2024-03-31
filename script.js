a780e5ff758ca8a48bdb5983c50aa71d78d5efae
Mahera Nayan <136584376+itznayan@users.noreply.github.com>
2024-03-31 17:32:42 +0530

    nw bg

--- script.js
+++ script.js
@@ -22,10 +22,70 @@ function openLinkInNewTab(url) {
 
 var typed = new Typed("#element", {
   strings: ["Frontend  Developer", " Android Developer"],
-  typeSpeed: 110,
+  typeSpeed: 130,
   loop: Infinity,
 });
 
-// Example usage
 var linkUrl = "https://www.example.com";
 openLinkInNewTab(linkUrl);
+
+//mouse-Pointer
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = ["black"];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+//background
+
+VANTA.BIRDS({
+  el: "#main",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 100.0,
+  minWidth: 100.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  backgroundColor: 0xfcfcfc,
+  colorMode: "lerp",
+  birdSize: 0.38,
+  wingSpan: 10.0,
+  separation: 22.0,
+  alignment: 22.0,
+  quantity: 6.0,
+});
